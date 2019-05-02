977ec58ae7710ac34a61860f603cb878c34d21bb
weiloon617 <chewweiloon007@gmail.com>
2019-05-02 11:50:26 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { connect } from 'react-redux'
+import { simpleAction } from './actions/simpleActions'
 
-function App() {
+import logo from './logo.svg'
+import './App.css'
+
+const mapStateToProps = state => ({
+  ...state,
+})
+
+const mapDispatchToProps = dispatch => ({
+  simpleAction: () => dispatch(simpleAction()),
+})
+
+function App(props) {
+  const simpleAction = () => props.simpleAction()
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,9 +31,14 @@ function App() {
         >
           Learn React
         </a>
+        <pre>{JSON.stringify(props)}</pre>
+        <button onClick={simpleAction}>Test redux action</button>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(App)
